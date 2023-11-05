edb4a399a84da17796f91f379548c8d041e62505
Alexey Vazhnov <vazhnov@boot-keys.org>
2023-11-05 22:33:02 +0100

    Return back `Dockerfile_alpine` instead of `Dockerfile_devuan` because of too much vulnerabilities found by Trivy

--- build_minikube.sh
+++ build_minikube.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o pipefail
 shopt -s dotglob
 
-minikube image build --file Dockerfile_devuan --tag 'python-flask-hello:devuan' .
-minikube image tag 'python-flask-hello:devuan' 'python-flask-hello:latest'
+minikube image build --file Dockerfile_alpine --tag 'python-flask-hello:alpine' .
+minikube image tag 'python-flask-hello:alpine' 'python-flask-hello:latest'
